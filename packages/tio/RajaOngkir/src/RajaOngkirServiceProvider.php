dd46da1f19d9e36acf70f25516dec9a1f7b4d914
Tio <sangvictim@gmail.com>
2021-09-23 16:38:39 +0700

    first commit

---
+++ packages/tio/RajaOngkir/src/RajaOngkirServiceProvider.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace tio\RajaOngkir;
+
+use Illuminate\Support\Facades\App;
+use Illuminate\Support\ServiceProvider;
+
+class RajaOngkirServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap the application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $this->publishes([
+            __DIR__ . '/config/rajaongkir.php' => config_path('config/rajaongkir.php'),
+        ]);
+    }
+
+    /**
+     * Register the application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->app->singleton('rajaongkir', function () {
+            return true;
+        });
+
+        App::bind('rajaongkir', function () {
+            return new Ongkir;
+        });
+    }
+}
